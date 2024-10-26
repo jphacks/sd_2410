39e16b51583018aacdbccdddd4110ac96a2f5238
yoshidaR3n <yoshida.ren.r3@dc.tohoku.ac.jp>
2024-10-26 12:12:38 +0900

    仮音声ボイス作成

--- Unity/create_voice.py
+++ Unity/create_voice.py
@@ -6,24 +6,32 @@ import time
 speaker_id = 3
 
 #ここにLLMからの応答データを入れる
-text = """明日6時半に起こして"""
+text = """わかったのだ！明日の朝６時に起こすのだ"""
 
 # 音声合成用クエリを作成
 query_payload = {'text': text, 'speaker': speaker_id}
 query_res = requests.post("http://127.0.0.1:50021/audio_query", params=query_payload)
+#query_res = requests.post("http://10.0.0.194:50021/audio_query", params=query_payload) #VPN接続して
 query_res.raise_for_status()
 audio_query = query_res.json()
 
 # 音声合成を実行して音声データを生成
 synthesis_res = requests.post(
-    "http://127.0.0.1:50021/synthesis", #リクエスト投げるときはyotchiのPCのIPアドレスにしてください
+    "http://127.0.0.1:50021/synthesis", #ローカルでやるとき
     params={'speaker': speaker_id},
     json=audio_query
 )
+
+# synthesis_res = requests.post(
+#     "http://10.0.0.194:50021/synthesis", #リクエスト投げるときはyotchiのPCのIPアドレスにしてください
+#     params={'speaker': speaker_id},
+#     json=audio_query
+# )
+
 synthesis_res.raise_for_status()
 
 # 音声ファイルを保存して再生
-with open("zundamon_voice.wav", "wb") as f:
+with open("sd_2410/Unity/Assets/zundamon_voice.wav", "wb") as f:
     f.write(synthesis_res.content)
 
 print("音声ファイルが生成されました！")
