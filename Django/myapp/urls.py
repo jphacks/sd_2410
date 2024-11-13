be5e92ec680def807959656a84385c04be803bb6
hayasaka11 <hayasaka.yoshiya.p7@dc.tohoku.ac.jp>
2024-11-13 21:51:00 +0900

    睡眠催促ようのサーバ側関数

--- Django/myapp/urls.py
+++ Django/myapp/urls.py
@@ -5,6 +5,7 @@ from .views.mp3_to_time import mp3_to_time
 from .views.image import image_view
 from .views.search import search
 from .views.send_image import send_image_with_text
+from .views.sleep_remind import sleep_remind
 
 urlpatterns = [
     path('openai-api/', openai_api_view, name='openai_api'),
@@ -12,5 +13,6 @@ urlpatterns = [
     path('mp3_openai/', mp3_to_time, name="mp3_api"),
     path('image_openai/', image_view, name="image_view"),
     path('search/', search, name="image_view"),
-    path('send_image/', send_image_with_text, name='send_image')
+    path('send_image/', send_image_with_text, name='send_image'),
+    path('sleep_remind/', sleep_remind, name='sleep_remind')
 ]
